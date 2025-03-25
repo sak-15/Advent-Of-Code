f5f650ba8f3eebc51f8c9c5c41cb4fdc8e1ce48e
Sakshi Gusain <sakshiii@Sakshis-Air.lan>
2025-03-25 13:32:00 +0100

    modified

--- challenge-level-01/solution_part-01.py
+++ challenge-level-01/solution_part-01.py
@@ -1,14 +1,17 @@
+# Function to calculate the total distance between two cordinates
 def calculate_total_distance(cordinates1, cordinates2):
-    cordinates1.sort()
-    cordinates2.sort()
+    cordinates1.sort() 
+    cordinates2.sort() 
     total_distance = 0
     for i in range(len(cordinates1)):
         total_distance += abs(cordinates1[i] - cordinates2[i])
     return total_distance
 
+# Initialize both the cordinates lists
 cordinates1 = []
 cordinates2 = []
 
+# Read the input from the file
 with open("input.txt", 'r') as file:
     for line in file:
         left, right = map(int, line.strip().split())
